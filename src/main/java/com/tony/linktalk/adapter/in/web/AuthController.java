0f81bbd8ab25530ad09865c0960e81b8fd5ea855
wlsdks <wlsdks12@naver.com>
2024-08-15 13:29:38 +0900

    :sparkles: 컨트롤러 작성

---
+++ src/main/java/com/tony/linktalk/adapter/in/web/AuthController.java
@@ -0,0 +1,12 @@
+package com.tony.linktalk.adapter.in.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 회원 인증 관련 API 컨트롤러
+ */
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/com/tony/linktalk/adapter/in/web/ChatController.java
@@ -0,0 +1,12 @@
+package com.tony.linktalk.adapter.in.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 채팅 관련 API 컨트롤러
+ */
+@RequestMapping("/chat")
+@RestController
+public class ChatController {
+}
