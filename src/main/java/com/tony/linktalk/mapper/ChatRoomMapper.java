49c0aa1dbd4ffa3aad80d937cd2a894bc2da3248
wlsdks <wlsdks12@naver.com>
2024-08-15 13:43:07 +0900

    :sparkles: mapstruct mapper 선언

---
+++ src/main/java/com/tony/linktalk/mapper/ChatRoomMapper.java
@@ -0,0 +1,12 @@
+package com.tony.linktalk.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface ChatRoomMapper {
+
+}
---
+++ src/main/java/com/tony/linktalk/mapper/MemberMapper.java
@@ -0,0 +1,13 @@
+package com.tony.linktalk.mapper;
+
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface MemberMapper {
+
+}
