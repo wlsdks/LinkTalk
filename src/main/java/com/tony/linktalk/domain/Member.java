72327186f141990a25ede0f74062658218762dc6
wlsdks <wlsdks12@naver.com>
2024-08-16 21:53:26 +0900

    :sparkles: 도메인 객체 선언

--- src/main/java/com/tony/linktalk/domain/Member.java
+++ src/main/java/com/tony/linktalk/domain/Member.java
@@ -1,29 +1,102 @@
 package com.tony.linktalk.domain;
 
-
+import com.tony.linktalk.exception.ErrorCode;
+import com.tony.linktalk.exception.LinkTalkException;
 import lombok.*;
+import org.springframework.util.ObjectUtils;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor
+import java.time.LocalDateTime;
+
+/**
+ * 회원
+ */
 @Builder
-@Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
-    private Long memberId;
-    private String username;
-    private String nickname;
-    private String email;
-    private String password;
+    private Long id;               // PK
+    private String email;          // 이메일
+    private String password;       // 소셜 로그인 사용자는 NULL일 수 있음
+    private String name;           // 이름
+    private String nickname;       // 닉네임
+    private String profilePictureUrl; // 프로필 사진
+    private String introduction;   // 자기소개
+    private String phoneNumber;    // 전화번호
+    private String address;        // 주소
+    private LocalDateTime birthDate; // 생년월일
+    private String gender;         // 성별
+    private String website;        // 웹사이트
+    private String statusMessage;  // 상태 메시지
+    private String accountStatus;  // 계정 상태 (예: 활성화, 비활성화, 정지 등)
+    private LocalDateTime joinedDate; // 가입일
+    private LocalDateTime lastLogin;  // 마지막 로그인 시간
+    private Jwt jwt;                  // JWT Object 객체
 
     // factory method
-    public static Member of(String username, String nickname, String email, String password) {
+    public static Member of(long id) {
         return Member.builder()
-                .username(username)
-                .nickname(nickname)
-                .email(email)
-                .password(password)
+                .id(id)
                 .build();
+    }
+
+
+    // factory method
+    public static Member of(Long id, String email, String name, LocalDateTime lastLogin) {
+        return Member.builder()
+                .id(id)
+                .email(email)
+                .name(name)
+                .lastLogin(lastLogin)
+                .build();
+    }
+
+
+    /**
+     * 이메일을 변경합니다.
+     *
+     * @param email 이메일
+     */
+    public void changeEmail(String email) {
+        if (ObjectUtils.isEmpty(email)) {
+            throw new LinkTalkException(ErrorCode.CHANGE_EMAIL_VALUE_NOT_FOUND);
+        }
+        this.email = email;
+    }
+
+
+    /**
+     * JWT 객체를 변경합니다.
+     *
+     * @param jwt JWT 객체
+     */
+    public void changeMemberInsideJwt(Jwt jwt) {
+        if (!ObjectUtils.isEmpty(this.jwt)) {
+            throw new LinkTalkException(ErrorCode.MEMBER_JWT_ALREADY_EXIST);
+        }
+        this.jwt = jwt;
+    }
+
+
+    /**
+     * 비밀번호를 변경합니다.
+     *
+     * @param encode 암호화된 비밀번호
+     */
+    public void changePassword(String encode) {
+        this.password = encode;
+    }
+
+
+    /**
+     * 회원 가입시 필수 값 체크
+     */
+    public void validSignUpMemberData() {
+        // 회원 가입시 필수 값 체크
+        if (email == null || password == null || name == null) {
+            throw new LinkTalkException(ErrorCode.MEMBER_REQUIRED_VALUE);
+        }
     }
 
 }
---
+++ src/main/java/com/tony/linktalk/domain/RefreshToken.java
@@ -0,0 +1,88 @@
+package com.tony.linktalk.domain;
+
+import com.tony.linktalk.exception.ErrorCode;
+import com.tony.linktalk.exception.LinkTalkException;
+import lombok.*;
+import org.springframework.util.ObjectUtils;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * 리프레시 토큰
+ */
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefreshToken {
+
+    private Long id;
+    private Member member;
+    private String token;
+    private LocalDateTime expiryDate;
+
+
+    // factory method
+    public static RefreshToken of(String requestRefreshToken) {
+        return RefreshToken.builder()
+                .token(requestRefreshToken)
+                .build();
+    }
+
+
+    // factory method
+    public static RefreshToken of(Member member, long refreshTokenDurationMinutes) {
+        return RefreshToken.builder()
+                .member(member)
+                .token(UUID.randomUUID().toString())
+                .expiryDate(LocalDateTime.now().plusMinutes(refreshTokenDurationMinutes))
+                .build();
+    }
+
+
+    /**
+     * @apiNote 토큰의 유효성 검증 (회원 정보, 만료 날짜 존재여부, 만료 기간 확인)
+     */
+    public void validRefreshToken() {
+        // 토큰 내부 회원 정보 확인
+        validTokenInnerMember();
+
+        // 토큰 내부 만료 날짜 확인
+        validTokenExpiryDateExist();
+
+        // 토큰의 "만료 기간"이 "현재 시간"보다 이전이면 만료된 토큰이니 true를 반환하고 아니면 예외 발생
+        validTokenExpiryDateExpiration();
+    }
+
+
+    /**
+     * @apiNote 토큰에 회원 정보가 존재하는지 확인
+     */
+    private void validTokenInnerMember() {
+        if (this.member == null) {
+            throw new LinkTalkException(ErrorCode.REFRESH_TOKEN_NOT_HAVE_MEMBER);
+        }
+    }
+
+
+    /**
+     * @apiNote 토큰 내부의 만료 날짜가 존재하는지 확인
+     */
+    private void validTokenExpiryDateExist() {
+        if (ObjectUtils.isEmpty(this.expiryDate)) {
+            throw new LinkTalkException(ErrorCode.REFRESH_TOKEN_EXPIRATION_DATE_NOT_FOUND);
+        }
+    }
+
+
+    /**
+     * @apiNote 리프레시 토큰이 만료되었는지 확인
+     */
+    private void validTokenExpiryDateExpiration() {
+        if (this.expiryDate.isBefore(LocalDateTime.now())) {
+            throw new LinkTalkException(ErrorCode.REFRESH_TOKEN_EXPIRED);
+        }
+    }
+
+}
