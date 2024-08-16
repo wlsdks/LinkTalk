b2399f8db4af31b52fd63f9f6555724697ed5bfd
wlsdks <wlsdks12@naver.com>
2024-08-17 01:14:02 +0900

    :memo: 잘린 주석 재작성

--- src/main/java/com/tony/linktalk/application/service/auth/AuthService.java
+++ src/main/java/com/tony/linktalk/application/service/auth/AuthService.java
@@ -33,7 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * 회원 인증 관련 비즈니스 로직을 처리하는 서비스 클래스
  * 회원 인증, JWT 관련 로직을 처리합니다.
- * Auth service는 member와
+ * Auth service는 member와 jwt를 다루는 비즈니스 로직을 처리합니다.
  */
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
