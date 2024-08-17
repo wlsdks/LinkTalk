dcfd916a38ae8b02250579fb276a685428489522
wlsdks <wlsdks12@naver.com>
2024-08-17 23:05:13 +0900

    :sparkles: useCase, persistenceAdapter 커스텀 어노테이션 선언

---
+++ src/main/java/com/tony/linktalk/util/custom/UseCase.java
@@ -0,0 +1,15 @@
+package com.tony.linktalk.util.custom;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)           // Class, Interface, Enum
+@Retention(RetentionPolicy.RUNTIME) // Runtime 시에도 유지
+@Component                          // Spring Bean으로 등록
+public @interface UseCase {
+    String value() default "";
+}
