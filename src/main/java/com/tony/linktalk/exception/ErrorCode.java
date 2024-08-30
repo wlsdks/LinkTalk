89b5fded4e1ad7ed3aa425f792205f91d0cc82e8
wlsdks <wlsdks12@naver.com>
2024-08-30 23:58:54 +0900

    :recycle: 에러코드 1차 정의 (추가 수정 필요)

--- src/main/java/com/tony/linktalk/exception/ErrorCode.java
+++ src/main/java/com/tony/linktalk/exception/ErrorCode.java
@@ -7,45 +7,29 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    // authentication errors
-    MEMBER_NOT_FOUND("MEM001", "Member not found"),
-    MEMBER_ALREADY_EXISTS("MEM002", "User already exists"),
-    INVALID_USER_ID("MEM003", "Invalid user ID"),
-    INVALID_USER_NAME("MEM004", "Invalid user name"),
-    INVALID_USER_EMAIL("MEM005", "Invalid user getEmail"),
-    INVALID_USER_PHONE("MEM006", "Invalid user phone"),
+    // common errors
+    UNEXPECTED_ERROR("LINK99999", "Unexpected error"),
 
-    // error codes for unexpected errors
-    UNEXPECTED_ERROR("ERR001", "Unexpected error"),
-    CHANGE_EMAIL_VALUE_NOT_FOUND("ERR002", "Change getEmail value not found"),
-    MEMBER_JWT_ALREADY_EXIST("ERR003", "Member JWT already exist"),
-    MEMBER_REQUIRED_VALUE("MEM007", "Member required value"),
+    // member
+    MEMBER_NOT_FOUND("LINK00001", "Member not found"),
+    MEMBER_JWT_ALREADY_EXIST("LINK00002", "Member JWT already exist"),
+    MEMBER_REQUIRED_VALUE("LINK00003", "Member required value"),
+    MEMBER_EMAIL_NOT_MATCH("LINK00004", "Member getEmail not match"),
+    MEMBER_INNER_EMAIL_NOT_FOUND("LINK00005", "Member inner getEmail not found"),
+    MEMBER_EMAIL_PARAM_NOT_FOUND("LINK00006", "Member getEmail param not found"),
+    CHANGE_EMAIL_VALUE_NOT_FOUND("LINK00007", "Change getEmail value not found"),
 
-
-    // refresh token errors
+    // refresh token
     REFRESH_TOKEN_NOT_HAVE_MEMBER("REF001", "Refresh token does not have a member"),
     REFRESH_TOKEN_EXPIRATION_DATE_NOT_FOUND("REF002", "Refresh token expiration date not found"),
     REFRESH_TOKEN_EXPIRED("REF003", "Refresh token expired"),
-
-    // data not found errors
     DATA_NOT_FOUND("DATA001", "Data not found"),
-
-    // authentication errors
-    AUTHENTICATION_NOT_FOUND("AUTH001", "Authentication not found"),
-
-
-    // member errors
-    MEMBER_EMAIL_NOT_MATCH("MEM008", "Member getEmail not match"),
-    MEMBER_INNER_EMAIL_NOT_FOUND("MEM009", "Member inner getEmail not found"),
-    MEMBER_EMAIL_PARAM_NOT_FOUND("MEM010", "Member getEmail param not found"),
-
 
     // file errors
     FILE_UPLOAD_ERROR("FILE001", "File upload error"),
     FILE_DOWNLOAD_ERROR("FILE002", "File download error"),
     FILE_NOT_FOUND("FILE003", "File not found"),
     INVALID_FILE_TYPE("FILE004", "Invalid file type"),
-
 
     // chat room errors
     CHAT_MESSAGE_ID_NOT_FOUND("CHAT001", "Chat message ID not found"),
@@ -58,7 +42,6 @@ public enum ErrorCode {
     PRINCIPAL_CAST_ERROR("PRINCIPAL_CAST_ERROR", "Principal cast error"),
     SECURITY_USER_NOT_FOUND("SEC001", "Security user not found"),
     PASSWORD_IS_NOT_HANDLE_IN_SECURITY_USER_DETAILS("SEC002", "Password is not handle in security user details"),
-
 
     ;
 
