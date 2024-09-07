63378619978a1b43229da7b01c2df9c961e0a67b
wlsdks <wlsdks12@naver.com>
2024-09-07 23:38:26 +0900

    :sparkles: 게시글 생성 로직 1차적으로 모양만 잡아둠

--- src/main/java/com/tony/linktalk/application/port/in/post/CreatePostUseCase.java
+++ src/main/java/com/tony/linktalk/application/port/in/post/CreatePostUseCase.java
@@ -1,6 +1,7 @@
 package com.tony.linktalk.application.port.in.post;
 
 import com.tony.linktalk.adapter.in.web.dto.response.post.PostResponseDto;
+import com.tony.linktalk.application.command.post.CreatePostCommand;
 
 public interface CreatePostUseCase {
 
--- src/main/java/com/tony/linktalk/application/service/post/CreatePostService.java
+++ src/main/java/com/tony/linktalk/application/service/post/CreatePostService.java
@@ -1,6 +1,9 @@
 package com.tony.linktalk.application.service.post;
 
+import com.tony.linktalk.adapter.in.web.dto.response.post.PostResponseDto;
+import com.tony.linktalk.application.command.post.CreatePostCommand;
 import com.tony.linktalk.application.port.in.post.CreatePostUseCase;
+import com.tony.linktalk.application.port.out.post.CreatePostPort;
 import com.tony.linktalk.util.custom.UseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +12,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @UseCase
 public class CreatePostService implements CreatePostUseCase {
+
+    private final CreatePostPort createPostPort;
+
+
+    /**
+     * @param command 게시글 생성 요청
+     * @return 게시글 생성 결과
+     * @apiNote 게시글 생성
+     */
+    @Transactional
+    @Override
+    public PostResponseDto createPost(CreatePostCommand command) {
+
+
+        return null;
+    }
+
 }
