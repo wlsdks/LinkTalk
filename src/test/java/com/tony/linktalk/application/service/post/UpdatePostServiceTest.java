ab16b1f5d17162eba256a3ef9277119972d29c77
wlsdks <wlsdks12@naver.com>
2024-09-12 23:06:58 +0900

    :sparkles: 테스트 코드 구조 만들기

---
+++ src/test/java/com/tony/linktalk/application/service/post/UpdatePostServiceTest.java
@@ -0,0 +1,36 @@
+package com.tony.linktalk.application.service.post;
+
+import com.tony.linktalk.application.port.out.post.UpdatePostPort;
+import com.tony.linktalk.mapper.PostMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UpdatePostServiceTest {
+
+    @Autowired
+    private UpdatePostPort updatePostPort;
+
+    @Autowired
+    private PostMapper postMapper;
+
+    @Autowired
+    private UpdatePostService sut;
+
+    @DisplayName("[happy] 게시글 수정 성공")
+    @Test
+    void updatePost() {
+        //given
+
+        //when
+        sut.updatePost(null);
+
+        //then
+
+    }
+
+}
