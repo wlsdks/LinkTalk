b2212e76298f77deb54f7f79c849b6dc10f190ae
wlsdks <wlsdks12@naver.com>
2024-09-20 23:10:46 +0900

    :sparkles: 런타임 에러 예외처리 핸들러 코드 추가

--- src/main/java/com/tony/linktalk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tony/linktalk/exception/GlobalExceptionHandler.java
@@ -33,4 +33,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
+    /**
+     * 애플리케이션에서 발생하는 모든 RuntimeException을 처리하기 위해 사용됩니다.
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<String>> handleRuntimeException(RuntimeException e) {
+        ApiResponse<String> response = ApiResponse.fail(ErrorCode.UNEXPECTED_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
