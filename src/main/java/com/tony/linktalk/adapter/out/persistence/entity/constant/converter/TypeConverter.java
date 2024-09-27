75cbebcdb392f0d42d01232ff60583572bedd1d5
wlsdks <wlsdks12@naver.com>
2024-09-27 23:07:28 +0900

    :recycle: 컨버터 타입 적용 테스트

--- src/main/java/com/tony/linktalk/adapter/out/persistence/entity/constant/converter/TypeConverter.java
+++ src/main/java/com/tony/linktalk/adapter/out/persistence/entity/constant/converter/TypeConverter.java
@@ -1,19 +1,19 @@
 package com.tony.linktalk.adapter.out.persistence.entity.constant.converter;
 
+import com.tony.linktalk.adapter.out.persistence.entity.constant.post.MediaType;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
-public class TypeConverter implements AttributeConverter {
+public class TypeConverter implements AttributeConverter<MediaType, MediaType> {
 
     @Override
-    public Object convertToDatabaseColumn(Object object) {
+    public MediaType convertToDatabaseColumn(MediaType mediaType) {
         return null;
     }
 
     @Override
-    public Object convertToEntityAttribute(Object object) {
+    public MediaType convertToEntityAttribute(MediaType mediaType) {
         return null;
     }
-
 }
